a6fa473ffb27af255b75952780c75b6e2cd6686a
anubhavmalhotra0395 <anubhavmalhotra0395@users.noreply.github.com>
2024-09-03 13:50:16 +0000

    deploy: 506921e55569a29ba37362d6e64ec632ec391dd6

--- assets/index-DlUXvCuW.js
+++ assets/index-DlUXvCuW.js
@@ -1,4 +1,4 @@
-import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const h=()=>`
+import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const y=()=>`
     <div class="py-5 position-relative contact-con" style="height:65vh; width:100vw;">
     <div class="wrapper centered">
       <article class="letter">
@@ -28,7 +28,7 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
     <div class="main-bg" style=" background-color:black;">
 <small class="text-center text-dark" style="font-size:10px;">@zarahrealestate 2024</small>
     </div>
-    `;function y(){document.body.classList.add("sent")}window.onload=()=>{const i=document.getElementById("sendLetter");i&&i.addEventListener("click",y)};const u="/real-estate/image/orla4.jpg",p="/real-estate/image/ava4.jpg",f="/real-estate/image/binyan4.jpg",x=()=>{let i=document.getElementById("next"),s=document.getElementById("prev"),l=document.querySelector(".carousel"),n=l.querySelector(".carousel .list"),e=document.querySelector(".carousel .thumbnail"),t=e.querySelectorAll(".item");document.querySelector(".carousel .time"),e.appendChild(t[0]);let o=3e3,c=7e3;i.onclick=function(){v("next")},s.onclick=function(){v("prev")};let d,m=setTimeout(()=>{next.click()},c);function v(g){let a=n.querySelectorAll(".carousel .list .item"),r=document.querySelectorAll(".carousel .thumbnail .item");g==="next"?(n.appendChild(a[0]),e.appendChild(r[0]),l.classList.add("next")):(n.prepend(a[a.length-1]),e.prepend(r[r.length-1]),l.classList.add("prev")),clearTimeout(d),d=setTimeout(()=>{l.classList.remove("next"),l.classList.remove("prev")},o),clearTimeout(m),m=setTimeout(()=>{next.click()},c)}},b=()=>`
+    `,u="/real-estate/image/orla4.jpg",p="/real-estate/image/ava4.jpg",f="/real-estate/image/binyan4.jpg",g=()=>{let n=document.getElementById("next"),l=document.getElementById("prev"),i=document.querySelector(".carousel"),s=i.querySelector(".carousel .list"),e=document.querySelector(".carousel .thumbnail"),t=e.querySelectorAll(".item");document.querySelector(".carousel .time"),e.appendChild(t[0]);let a=3e3,c=7e3;n.onclick=function(){v("next")},l.onclick=function(){v("prev")};let d,m=setTimeout(()=>{next.click()},c);function v(h){let r=s.querySelectorAll(".carousel .list .item"),o=document.querySelectorAll(".carousel .thumbnail .item");h==="next"?(s.appendChild(r[0]),e.appendChild(o[0]),i.classList.add("next")):(s.prepend(r[r.length-1]),e.prepend(o[o.length-1]),i.classList.add("prev")),clearTimeout(d),d=setTimeout(()=>{i.classList.remove("next"),i.classList.remove("prev")},a),clearTimeout(m),m=setTimeout(()=>{next.click()},c)}},x=()=>`
     <div class="list w-100">
     <div class="item w-100">
         <img src=${u}>
@@ -98,31 +98,31 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
 </div>
 
 <div class="time"></div>
-`,A="/real-estate/image/zarah5.jpeg",w=()=>`
+`,b="/real-estate/image/zarah5.jpeg",A=()=>`
     <div class="d-flex py-5" style="background-color:#f8f3ea">
   
     <div class="col-4 d-flex justify-content-center align-items-center text-center">
    <h1 class="about-header  w-100 ">ABOUT ME</h1>
     </div>
     <div class="col-4">
-    <img src="${A}" class="img-fluid"/>
+    <img src="${b}" class="img-fluid"/>
     </div>
     <div class="col-4 d-flex justify-content-center align-items-center text-center  px-5 ">
     <h1 class="about-des  w-100 ">"Passionate about helping clients find their perfect home or investment. With a keen eye for market trends and a deep understanding of property values, I tur real estate dreams into reality, one deal at a time.My commitment is to provide personalized service, ensuring every client finds a property that truly fits their lifestyle and goals."</h1>
     </div>
-    </div>`,E="/real-estate/image/il1.png",I="/real-estate/image/il3.png",L="/real-estate/image/il4.png",S=()=>`
-<div class="d-flex justify-content-center align-items-center py-5" style="height:45vh;">
+    </div>`,w="/real-estate/image/il1.png",L="/real-estate/image/il3.png",E="/real-estate/image/il4.png",I=()=>`
+<div class="d-flex justify-content-center align-items-center py-5" style="height:30vh;">
 <div class="d-flex col-12 justify-content-center align-items-center">
 <div class="col-2 d-flex flex-column">
-<img src="${E}" class="img-fluid">
-<h3></h3>
-</div>
-<div class="col-2 d-flex flex-column">
-<img src="${I}" class="img-fluid">
+<img src="${w}" class="img-fluid">
 <h3></h3>
 </div>
 <div class="col-2 d-flex flex-column">
 <img src="${L}" class="img-fluid">
+<h3></h3>
+</div>
+<div class="col-2 d-flex flex-column">
+<img src="${E}" class="img-fluid">
 <h3></h3>
 </div>
 </div>
@@ -131,13 +131,13 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
       <header></header>
       <div class="d-flex justify-content-center align-items-center flex-column">
         <div class="carousel">
-          ${b()}
+          ${x()}
         </div>
         <div class="d-flex justify-content-center align-items-center w-100" style="height:65vh;">
           <div class="main-text">ZARAH LENZ</div>
         </div>
-        <div>${w()}</div>
-        ${S()}
-        <div>${h()}</div>
+        <div>${A()}</div>
+        ${I()}
+        <div>${y()}</div>
       </div>
-    `;const i=document.querySelectorAll("img");let s=0;i.length===0?(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block"):i.forEach(l=>{l.onload=()=>{s++,s===i.length&&(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block")},l.onerror=()=>{s++,s===i.length&&(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block")}})});x();
+    `;const n=document.querySelectorAll("img");let l=0;n.length===0?(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",g()):n.forEach(s=>{s.onload=s.onerror=()=>{l++,l===n.length&&(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",g())}});const i=document.getElementById("sendLetter");i&&i.addEventListener("click",S)});function S(){document.body.classList.add("sent")}
