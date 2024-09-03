37c025831125aaf02f9b50f8d89d67cf20fd7a81
anubhavmalhotra0395 <anubhavmalhotra0395@users.noreply.github.com>
2024-09-03 15:57:02 +0000

    deploy: 54256525ca3fb79942fd6eb6cc06ccc58f6043f4

--- assets/index-gpZpTF4W.js
+++ assets/index-gpZpTF4W.js
@@ -1,20 +1,23 @@
-import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const y=()=>`
+(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const y=()=>`
     <div class="py-5 position-relative contact-con" style="height:65vh; width:100vw;">
     <div class="wrapper centered">
-      <article class="letter">
+      <article class="letter ">
         <div class="side">
-          <h1>Get in Touch</h1>
+          <h1>Book an Appointment</h1>
           <p>
             <textarea placeholder="Your message"></textarea>
           </p>
         </div>
-        <div class="side">
+        <div class="side ">
           <p>
             <input type="text" placeholder="Your name" >
           </p>
           <p>
             <input type="email" placeholder="Your email" >
           </p>
+          <p>
+          <input type="date" placeholder="Fix Dates" >
+        </p>
           <p>
             <button id="sendLetter">Send</button>
           </p>
@@ -25,17 +28,17 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
     </div>
     <p class="result-message centered text-center">Thank you for your message</p>
     </div>
-    <div class="main-bg" style=" background-color:black;">
-<small class="text-center text-dark" style="font-size:10px;">@zarahrealestate 2024</small>
+    <div class="main-bg" style=" background-color:black;border:30px solid white; border-top:0px;">
+<small class="text-center text-light" style="font-size:10px;">@zarahrealestate 2024</small>
     </div>
-    `,u="/real-estate/image/orla4.jpg",p="/real-estate/image/ava4.jpg",f="/real-estate/image/binyan4.jpg",g=()=>{let n=document.getElementById("next"),l=document.getElementById("prev"),i=document.querySelector(".carousel"),s=i.querySelector(".carousel .list"),e=document.querySelector(".carousel .thumbnail"),t=e.querySelectorAll(".item");document.querySelector(".carousel .time"),e.appendChild(t[0]);let a=3e3,c=7e3;n.onclick=function(){v("next")},l.onclick=function(){v("prev")};let d,m=setTimeout(()=>{next.click()},c);function v(h){let r=s.querySelectorAll(".carousel .list .item"),o=document.querySelectorAll(".carousel .thumbnail .item");h==="next"?(s.appendChild(r[0]),e.appendChild(o[0]),i.classList.add("next")):(s.prepend(r[r.length-1]),e.prepend(o[o.length-1]),i.classList.add("prev")),clearTimeout(d),d=setTimeout(()=>{i.classList.remove("next"),i.classList.remove("prev")},a),clearTimeout(m),m=setTimeout(()=>{next.click()},c)}},x=()=>`
+    `,u="/real-estate/image/orla4.jpg",p="/real-estate/image/ava4.jpg",f="/real-estate/image/binyan4.jpg",h=()=>{let n=document.getElementById("next"),l=document.getElementById("prev"),i=document.querySelector(".carousel"),s=i.querySelector(".carousel .list"),e=document.querySelector(".carousel .thumbnail"),t=e.querySelectorAll(".item");document.querySelector(".carousel .time"),e.appendChild(t[0]);let o=3e3,c=7e3;n.onclick=function(){v("next")},l.onclick=function(){v("prev")};let d,m=setTimeout(()=>{next.click()},c);function v(g){let r=s.querySelectorAll(".carousel .list .item"),a=document.querySelectorAll(".carousel .thumbnail .item");g==="next"?(s.appendChild(r[0]),e.appendChild(a[0]),i.classList.add("next")):(s.prepend(r[r.length-1]),e.prepend(a[a.length-1]),i.classList.add("prev")),clearTimeout(d),d=setTimeout(()=>{i.classList.remove("next"),i.classList.remove("prev")},o),clearTimeout(m),m=setTimeout(()=>{next.click()},c)}},x=()=>`
     <div class="list w-100">
     <div class="item w-100">
         <img src=${u}>
         <div class="content w-100">
         <div class="author">AVA</div>
         <div class="title">PALM JUMEIRAH</div>
-            <div class="topic">D. COLLECTION</div>
+            <div class="topic">DOR. COLLECTION</div>
             <div class="text-center d-flex justify-content-center">
             <p class="des w-50">
             "Discover the epitome of luxury living in Dubai's booming real estate market. From waterfront villas to high-rise apartments, find your dream home or next investment in one of the world's most sought-after cities."</p>
@@ -99,7 +102,7 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
 
 <div class="time"></div>
 `,b="/real-estate/image/zarah5.jpeg",A=()=>`
-    <div class="d-flex py-5" style="background-color:#f8f3ea">
+    <div class="d-flex py-5" style="background-color:#f8f3ea;border: 30px solid white;">
   
     <div class="col-4 d-flex justify-content-center align-items-center text-center">
    <h1 class="about-header  w-100 ">ABOUT ME</h1>
@@ -111,17 +114,17 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
     <h1 class="about-des  w-100 ">"Passionate about helping clients find their perfect home or investment. With a keen eye for market trends and a deep understanding of property values, I tur real estate dreams into reality, one deal at a time.My commitment is to provide personalized service, ensuring every client finds a property that truly fits their lifestyle and goals."</h1>
     </div>
     </div>`,w="/real-estate/image/il1.png",L="/real-estate/image/il3.png",E="/real-estate/image/il4.png",I=()=>`
-<div class="d-flex justify-content-center align-items-center py-5" style="height:30vh;">
+<div class="d-flex justify-content-center align-items-center py-5" style="height:65vh;">
 <div class="d-flex col-12 justify-content-center align-items-center">
-<div class="col-2 d-flex flex-column">
+<div class="col-4 d-flex flex-column">
 <img src="${w}" class="img-fluid">
 <h3></h3>
 </div>
-<div class="col-2 d-flex flex-column">
+<div class="col-4 d-flex flex-column">
 <img src="${L}" class="img-fluid">
 <h3></h3>
 </div>
-<div class="col-2 d-flex flex-column">
+<div class="col-4 d-flex flex-column">
 <img src="${E}" class="img-fluid">
 <h3></h3>
 </div>
@@ -130,7 +133,7 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
 `;window.addEventListener("load",()=>{document.querySelector("#main-content").innerHTML=`
       <header></header>
       <div class="d-flex justify-content-center align-items-center flex-column">
-        <div class="carousel">
+        <div class="carousel" style="border: 30px solid white;">
           ${x()}
         </div>
         <div class="d-flex justify-content-center align-items-center w-100" style="height:65vh;">
@@ -140,4 +143,4 @@ import"https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js";(fu
         ${I()}
         <div>${y()}</div>
       </div>
-    `;const n=document.querySelectorAll("img");let l=0;n.length===0?(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",g()):n.forEach(s=>{s.onload=s.onerror=()=>{l++,l===n.length&&(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",g())}});const i=document.getElementById("sendLetter");i&&i.addEventListener("click",S)});function S(){document.body.classList.add("sent")}
+    `;const n=document.querySelectorAll("img");let l=0;n.length===0?(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",h()):n.forEach(s=>{s.onload=s.onerror=()=>{l++,l===n.length&&(document.getElementById("loading-screen").style.display="none",document.getElementById("main-content").style.display="block",h())}});const i=document.getElementById("sendLetter");i&&i.addEventListener("click",D)});function D(){document.body.classList.add("sent")}
