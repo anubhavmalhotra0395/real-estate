a6f2e14a55abdb49c1d59077ef2a12ca469b6ccc
anubhavmalhotra0395 <malhotraanubhav0395@gmail.com>
2024-09-05 15:31:36 +0530

    latest

--- achievements.js
+++ achievements.js
@@ -26,15 +26,3 @@ export const achievementsHtml = () => {
 
     `
 }
-
-{/* <div class="d-flex col-12 px-5 gap-5 py-5 justify-content-center align-items-center">
-<div class="col-8 d-flex flex-column   px-5">
-<h1 class="about-header2 mb-5">Success in real estate starts when you believe you are worthy of it.</h1>
-    <h1 class="about-des3  w-100">"1.Passionate about helping clients find their perfect home or investment. With a keen eye for market trends and a deep understanding of property values, I turn real estate dreams into reality, one deal at a time. My commitment is to provide personalized service, ensuring every client finds a property that truly fits their lifestyle and goals."</h1><br/>
-    <h1 class="about-des3 w-100">"2.Passionate about helping clients find their perfect home or investment. With a keen eye for market trends and a deep understanding of property values, I turn real estate dreams into reality, one deal at a time. My commitment is to provide personalized service, ensuring every client finds a property that truly fits their lifestyle and goals."</h1><br/>
-    <h1 class="about-des3  w-100">"3.Passionate about helping clients find their perfect home or investment. With a keen eye for market trends and a deep understanding of property values, I turn real estate dreams into reality, one deal at a time. My commitment is to provide personalized service, ensuring every client finds a property that truly fits their lifestyle and goals."</h1><br/>
-</div>
-<div class="col-4">
-    <img src="${AchImg}" class="img-fluid" alt="Achievements Image" />
-</div>
-</div> */}
--- hero.js
+++ hero.js
@@ -10,7 +10,9 @@ export const heroHtml = () => {
     <div class="col-7  mb-5 " >
    <h1 class="hero-header">Lets Find Your Dream Home, I'm Here to Help</h1>
    <p class="hero-des mt-4 ">Discover your dream property in the UAE with our expert real estate services. Whether you're looking for luxury villas, modern apartments, or commercial spaces, we offer a curated selection of properties across prime locations in the UAE. Let us help you find the perfect place to live, invest, or grow your business.</p>
-
+<div >
+<button class="button-hero ">GET IN TOUCH</button>
+</div>
     </div>
     </div>
     </div>
